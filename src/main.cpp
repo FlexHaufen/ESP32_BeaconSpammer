42dec5e07cb59c8510ed69c1299557a5726914ef
FlexHaufen <ibims1k0tt@gmail.com>
2023-03-15 17:40:25 +0100

    Added content

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file main.cpp
+ * @author flexhaufen
+ * @brief ESP_BeaconSpammer
+ * 
+ * @version 0.1
+ * @date 2023-03-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+// *** INCLUDES ***
+#include <Arduino.h>
+
+// for WiFi
+#include <WiFi.h>
+#include <esp_wifi.h>
+
+// external
+#include <vector>
+#include <array>
+
+
+// ** DEFINES ***
+
+// *** GLOABL VARIABLES ***
+
+
+
+/**
+ * @brief Setup
+ * 
+ */
+void setup() {
+
+}
+
+/**
+ * @brief Loop
+ * 
+ */
+void loop() {
+
+}
